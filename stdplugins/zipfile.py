3dd517ccfa6062ddeb2c5b5e1374dd63dadb3b9f
SnapDragon <maliktahir0702@gmail.com>
2019-08-24 16:59:27 +0200

    Update zipfile.py

--- stdplugins/zipfile.py
+++ stdplugins/zipfile.py
@@ -28,26 +28,6 @@ async def _(event):
             )
         except Exception as e:  # pylint:disable=C0103,W0703
             await mone.edit(str(e))
-        while not downloader.isFinished():
-            total_length = downloader.filesize if downloader.filesize else None
-            downloaded = downloader.get_dl_size()
-            display_message = ""
-            now = time.time()
-            diff = now - c_time
-            percentage = downloader.get_progress() * 100
-            speed = downloader.get_speed()
-            elapsed_time = round(diff) * 1000
-            progress_str = "[{0}{1}]\nProgress: {2}%".format(
-                ''.join(["█" for i in range(math.floor(percentage / 5))]),
-                ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
-                round(percentage, 2))
-            estimated_total_time = downloader.get_eta(human=True)
-            try:
-                current_message = f"trying to download\nURL: {url}\nFile Name: {file_name}\n{progress_str}\n{humanbytes(downloaded)} of {humanbytes(total_length)}\nETA: {estimated_total_time}"
-                if round(diff % 10.00) == 0 and current_message != display_message:
-                    display_message = current_message
-            except Exception as e:
-                logger.info(str(e))
     directory_name = "{}".format(downloaded_file_name.replace("`", ""))
     zipf = zipfile.ZipFile(directory_name + ".zip", "w", zipfile.ZIP_DEFLATED)
     zipdir(directory_name, zipf)
