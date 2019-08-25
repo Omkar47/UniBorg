815e610fa221375bb3085190e008f32a569cf0d6
SnapDragon <maliktahir0702@gmail.com>
2019-08-25 16:54:47 +0200

    Update uniborg.py

--- uniborg/uniborg.py
+++ uniborg/uniborg.py
@@ -10,19 +10,17 @@ from telethon import TelegramClient
 import telethon.utils
 import telethon.events
 
-from .storage import Storage
 from . import hacks
 
 
 class Uniborg(TelegramClient):
     def __init__(
-            self, session, *, n_plugin_path="plugins", storage=None, db_plugin_path="plugins",
+            self, session, *, n_plugin_path="plugins", db_plugin_path="plugins",
             bot_token=None, api_config=None, **kwargs):
         self._name = "LoggedIn"
-        self.storage = storage or (lambda n: Storage(Path("data") / n))
         self._logger = logging.getLogger("UniBorg")
         self._plugins = {}
-        self._plugin_path = n_plugin_path
+        self.n_plugin_path = n_plugin_path
         self.db_plugin_path = db_plugin_path
         self.config = api_config
 
@@ -89,7 +87,7 @@ class Uniborg(TelegramClient):
 
 
     def load_plugin(self, shortname):
-        self.load_plugin_from_file(f"{self.a_plugin_path}/{shortname}.py")
+        self.load_plugin_from_file(f"{self.n_plugin_path}/{shortname}.py")
 
     def load_plugin_from_file(self, path):
         path = Path(path)
@@ -101,7 +99,6 @@ class Uniborg(TelegramClient):
 
         mod.borg = self
         mod.logger = logging.getLogger(shortname)
-        mod.storage = self.storage(f"{self._name}/{shortname}")
         # declare Config and tgbot to be accessible by all modules
         mod.Config = self.config
         if self.config.TG_BOT_USER_NAME_BF_HER is not None:
