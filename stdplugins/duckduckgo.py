cc7c4f2acfac65998e2e9731809fc09b0a1e9060
SnapDragon <maliktahir0702@gmail.com>
2019-08-26 20:58:19 +0200

    Update duckduckgo.py

--- stdplugins/duckduckgo.py
+++ stdplugins/duckduckgo.py
@@ -10,10 +10,9 @@ async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    sample_url = "https://da.gd/s?url=https://duckduckgo.com/?q={}".format(input_str.replace(" ","+"))
-    response_api = requests.get(sample_url).text
-    if response_api:
-        link = response_api.rstrip()
+    sample_url = "https://duckduckgo.com/?q={}".format(input_str.replace(" ","+"))
+    if sample_url:
+        link = sample_url.rstrip()
         await event.edit("Let me 🦆 DuckDuckGo that for you:\n🔎[{}]({})".format(input_str, link))
     else:
         await event.edit("something is wrong. please try again later.")
