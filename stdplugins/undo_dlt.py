f729a10fa6bf9af99f00840c5760b9df5c5ec050
SnapDragon <maliktahir0702@gmail.com>
2019-08-27 20:03:03 +0200

    Update undo_dlt.py

--- stdplugins/undo_dlt.py
+++ stdplugins/undo_dlt.py
@@ -8,7 +8,7 @@ async def _(event):
     if event.fwd_from:
         return
     c = await event.get_chat()
-    if c.admin_rights:
+    if c.admin_rights or c.creator:
         a = await borg.get_admin_log(event.chat_id,limit=1, search="", edit=False, delete=True)
         for i in a:
           await event.reply(i.original.action.message)
