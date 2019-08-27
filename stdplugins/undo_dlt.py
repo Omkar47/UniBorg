95b01ad9ad2818afa082f9f9070409326a45d427
SnapDragon <maliktahir0702@gmail.com>
2019-08-27 19:38:15 +0200

    Update undo_dlt.py

--- stdplugins/undo_dlt.py
+++ stdplugins/undo_dlt.py
@@ -2,10 +2,14 @@ from telethon import events
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="undlt")
+@borg.on(admin_cmd(pattern="undlt"))
 async def _(event):
     if event.fwd_from:
         return
-    a = await borg.get_admin_log(event.chat_id,limit=1, search="", edit=False, delete=True)
-    for i in a:
-      await event.reply(i.original.action.message)
+    c = await event.get_chat()
+    if (c.admin_rights):
+        a = await borg.get_admin_log(event.chat_id,limit=1, search="", edit=False, delete=True)
+        for i in a:
+          await event.reply(i.original.action.message)
+    else:
+        await event.edit("You need administrative permissions in order to do this command")
