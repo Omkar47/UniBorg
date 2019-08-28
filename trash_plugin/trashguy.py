01b13a2eeb072ad48209bf2914516c3fcd1f9545
SnapDragon <maliktahir0702@gmail.com>
2019-08-28 21:46:49 +0200

    Update trashguy.py

--- trash_plugin/trashguy.py
+++ trash_plugin/trashguy.py
@@ -1,9 +1,8 @@
-# coding: utf8
 # =================================================== #
 #                 Trash Guy Script                    #
-#                    🗑️(> ^_^)>                       #
+#                     (> ^_^)>                        #
 #              Made by Zac (t.me/Zacci)               #
-#                 Version 3.1.4-pi                    #
+#                 Version 3.2.0-pi                    #
 #         Donate:                                     #
 #         1CoRm4mKCUPs5XQnFVSVQ4xGMAp29pyYzC          #
 # =================================================== #
@@ -24,52 +23,58 @@
 import sys
 
 
-def main(input_default):
+def main():
     """Example usage of TrashGuy class."""
-    if input_default is None:
-        input_default = '🍓 🍅 🍊 🍋'.split()
-    else:
-        input_default = input_default.split()
+    # user_input = '🍓 🍅 🍊'
+    user_input = ' '.join(sys.argv[1:])
 
-    user_input = sys.argv[1:]
-    if not user_input:
-        user_input = input_default
+    trash_guy = TrashGuy(user_input, symbol_spacing=Symbols.SPACER_EMOJI)
 
-    trash_guy = TrashGuy(user_input).animate()
-    print(*trash_guy, sep='\n')
+    print(trash_guy)
+
+
+class Symbols:
+    SPACER_DEFAULT = '\u0020'
+    SPACER_WIDE = '\u2800\u0020'
+    SPACER_EMOJI = '➖'
+    DEFAULT_INPUT = '\u2622'
+    MONOSPACE_WRAPPER = '`'
+    TRASH_EMOJI = '\U0001F5D1'
+    GUY_LEFT = '<(^_^ <)'
+    GUY_RIGHT = '(> ^_^)>'
 
 
 class TrashGuy:
-    __SYMBOL_SPACE = u'\u0020'
-    __SYMBOL_TG_SPACE = u'\u2800\u0020'
-    __SYMBOL_DEFAULT = u'\u274C'
-
-    def __init__(self, user_input: list = None, **kwargs):
+    def __init__(self, user_input: str,
+                 symbol_trash: str = Symbols.TRASH_EMOJI,
+                 symbol_left: str = Symbols.GUY_LEFT,
+                 symbol_right: str = Symbols.GUY_RIGHT,
+                 symbol_spacing: str = Symbols.SPACER_DEFAULT,
+                 monospace_wrapper: str = Symbols.MONOSPACE_WRAPPER,
+                 wrap_monospace: bool = False):
         if not user_input:
-            self.user_input = [TrashGuy.__SYMBOL_DEFAULT]
+            self.user_input = [Symbols.DEFAULT_INPUT]
         else:
-            self.user_input = user_input
+            self.user_input = str(user_input).split()
 
-        options = {'symbol_trash': u'\U0001F5D1',
-                   'symbol_left': '<(^_^ <)',
-                   'symbol_right': '(> ^_^)>',
-                   'telegram': False}
+        self.symbol_trash = symbol_trash
+        self.symbol_left = symbol_left
+        self.symbol_right = symbol_right
+        self.symbol_spacing = symbol_spacing
+        self.monospace_wrapper = monospace_wrapper
+        self.wrap_monospace = wrap_monospace
 
-        options.update(kwargs)
-
-        self.symbol_trash = str(options['symbol_trash']).strip()
-        self.symbol_left = str(options['symbol_left']).strip()
-        self.symbol_right = str(options['symbol_right']).strip()
-        self.telegram = bool(options['telegram'])
+    def __str__(self):
+        return '\n'.join(frame for frame in self)
 
     def __add(self, frames: list, frame):
-        tick = ['`']
-        if self.telegram:
-            frames.append(''.join(tick + list(frame) + tick))
+        wrapper = [Symbols.MONOSPACE_WRAPPER]
+        if self.wrap_monospace:
+            frames.append(''.join(wrapper + list(frame) + wrapper))
         else:
             frames.append(''.join(list(frame)))
 
-    def animate(self) -> list:
+    def __iter__(self):
         """Dynamically generated frames of the animated trash guy using provided symbols."""
         trash_item_sequence = []
 
@@ -77,11 +82,10 @@ class TrashGuy:
         trash_length = len(trash_input)
         trash_reversed = reversed(trash_input)
 
-        sym_trash = self.symbol_trash
-        sym_left = self.symbol_left
-        sym_right = self.symbol_right
-
-        sym_space = TrashGuy.__SYMBOL_SPACE if not self.telegram else self.__SYMBOL_TG_SPACE
+        s_trash = self.symbol_trash
+        s_left = self.symbol_left
+        s_right = self.symbol_right
+        s_space = self.symbol_spacing
 
         trash_truncator = trash_length
         # The animation is created from right to left and bottom to up and then reversed at each frame step.
@@ -92,10 +96,10 @@ class TrashGuy:
         for item in trash_reversed:
             truncating_items = trash_input[:trash_truncator]
             truncated_len_difference = trash_length - trash_truncator
-            space_padding = [sym_space] * truncated_len_difference
+            space_padding = [s_space] * truncated_len_difference
 
             # Create a dynamic canvas while each item disappears
-            canvas = [*truncating_items + space_padding, *[sym_space] * 3, sym_trash]
+            canvas = [*truncating_items + space_padding, *[s_space] * 3, s_trash]
             r_canvas = list(reversed(canvas))
 
             last_item_index = -len(truncating_items)
@@ -103,44 +107,44 @@ class TrashGuy:
             # Loop over the canvas to animate the trash guy going right while holding the item.
             for index in range(len(r_canvas)):
                 # Trash guy hits the item he wants to pickup
-                if r_canvas[index] == sym_space or r_canvas[index] == item:
+                if r_canvas[index] == s_space or -index == last_item_index:
                     try:
-                        if r_canvas[index - 1] == sym_trash:
+                        if r_canvas[index - 1] == s_trash:
                             # Looking left on the very final frame of the animation
-                            r_canvas[index] = sym_left
+                            r_canvas[index] = s_left
 
                             # Temporarily remove the item from the trash pile while trash guy is holding it
                             if r_canvas[last_item_index] == item:
-                                r_canvas[last_item_index] = sym_space
+                                r_canvas[last_item_index] = s_space
 
                             # Snapshot last frame looking left
                             self.__add(frames, reversed(r_canvas))
 
                             # Set trash guy's previous position
-                            r_canvas[index] = sym_right
+                            r_canvas[index] = s_right
 
                             # Snapshot before-last frame
                             self.__add(frames, reversed(r_canvas))
 
                             # Remove trash guy from this position for next frame (which is the previous frame)
                             # (must be set to space character)
-                            r_canvas[index] = sym_space
+                            r_canvas[index] = s_space
 
                         # Animate trash guy going right with the item until he hits the trash can
-                        elif r_canvas[index - 1] == sym_space and r_canvas[index - 1] != sym_trash:
+                        elif r_canvas[index - 1] == s_space and r_canvas[index - 1] != s_trash:
                             r_canvas[index - 1] = item
-                            r_canvas[index] = sym_right
+                            r_canvas[index] = s_right
 
                             # Temporarily remove the item from the trash pile while trash guy is holding it
                             if r_canvas[last_item_index] == item:
-                                r_canvas[last_item_index] = sym_space
+                                r_canvas[last_item_index] = s_space
 
                             # Snapshot frame while moving towards trash can
                             self.__add(frames, reversed(r_canvas))
 
                             # Clear the last two spaces for the next frame
-                            r_canvas[index] = sym_space
-                            r_canvas[index - 1] = sym_space
+                            r_canvas[index] = s_space
+                            r_canvas[index - 1] = s_space
 
                     except IndexError:
                         pass  # animation reached end
@@ -148,15 +152,15 @@ class TrashGuy:
             # Loop over the canvas to animate trash guy going from the beginning of the animation towards the items
             for index in range(len(canvas)):
                 # if the guy is near the trash can, skip the frame
-                if canvas[index] == sym_space and canvas[index + 1] != sym_trash:
+                if canvas[index] == s_space and canvas[index + 1] != s_trash:
                     # animate guy starting from the first space after the one being next to the trash can
-                    canvas[index] = sym_left
+                    canvas[index] = s_left
 
                     # Snapshot the frames of the animation going left for as long as there are whitespaces
                     self.__add(frames, canvas)
 
                     # Remove the previous position and continue
-                    canvas[index] = sym_space
+                    canvas[index] = s_space
 
             # Append all frames in reverse order
             trash_item_sequence += list(reversed(frames))
@@ -164,7 +168,7 @@ class TrashGuy:
             # Truncate the items list by one item each loop
             trash_truncator -= 1
 
-        return trash_item_sequence
+        return iter(trash_item_sequence)
 
 
 if __name__ == '__main__':
