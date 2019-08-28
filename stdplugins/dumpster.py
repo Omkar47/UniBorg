0c0751b4df783eec4a621b99a9ddc9bb4e7a82d4
SnapDragon <maliktahir0702@gmail.com>
2019-08-28 14:20:00 +0200

    Update dumpster.py

--- stdplugins/dumpster.py
+++ stdplugins/dumpster.py
@@ -6,6 +6,6 @@ from uniborg.util import admin_cmd
 @borg.on(admin_cmd(pattern="dump ?(.*)"))
 async def _(message):
     from Plugins.trashguy import TrashGuy
-        obj = message.pattern_match.group(1)
-        if not obj:
-            obj = "💻 💻 💻"
+    obj = message.pattern_match.group(1)
+    if not obj:
+        obj = "💻 💻 💻"
