0592618ff9639a8ba55f9b4c8525251540136402
SnapDragon <maliktahir0702@gmail.com>
2019-08-30 17:38:16 +0200

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -112,3 +112,20 @@ Telethon : {}```
         __version__,len(borg._plugins),uptime_string,cpu,d.percent)
 
     await event.edit(start_string,link_preview=True)
+
+
+@borg.on(admin_cmd(pattern="syntax (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    plugin_name = event.pattern_match.group(1)
+    if plugin_name in borg._plugins:
+        help_string = borg._plugins[plugin_name].__doc__
+        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © @UniBorg"
+        if help_string:
+            plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"
+        else:
+            plugin_syntax = f"No DOCSTRING has been setup for {plugin_name} plugin."
+    else:
+        plugin_syntax = "Enter valid **Plugin** name.\nDo `.exec ls stdplugins` or `.helpme` to get list of valid plugin names."
+    await event.edit(plugin_syntax)
